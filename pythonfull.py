13d16a0d47b199478de22bfd55dd82f041e5a1d1
chandra prakash vaishnav <chandraprakashvaishnav2006@gmail.com>
2026-07-07 22:11:10 +0530

    2nd learn lesson

--- pythonfull.py
+++ pythonfull.py
@@ -1,2 +1,60 @@
-# this code for print hello 
-print("hello world")
+# # this code for print hello 
+# print("hello world")
+
+# # add two no
+# a=int(input("Enter first number: "))
+# b=int(input("enter second no"))
+# c=a+b
+# print("the sum is",c)
+
+
+# # to enter int in binary form
+# d=0b1010;
+# print(d)
+
+# to check boolean data type
+# code 1 has output 1
+# a=True
+# b=False
+# c=a+b
+# print(c)
+
+# code 2 has output True
+# a=10
+# b=20
+# c=a<b
+# print(c)
+
+
+# string print 
+# a="hello world"
+# print(a)
+
+
+# slicing of string
+k="Arav"
+
+# to print index 0
+# print(k[0]) 
+
+# # to print : full string
+# print(k[:])
+
+# #  it stat print index with index 1
+# print(k[1:])
+
+
+# # to print till text 
+# print(k[:2])
+
+
+# # to print text in reverse
+# print(k[::-1])
+
+
+# # to print length of string
+# print(len(k))
+
+# to print para
+# print('''arav
+# he is too good''')
