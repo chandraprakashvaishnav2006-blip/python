f775bd45a92cc6bed41f6008929f1b726a534b43
chandra prakash vaishnav <chandraprakashvaishnav2006@gmail.com>
2026-08-01 10:20:51 +0530

    more pattern practice

--- pythonfull.py
+++ pythonfull.py
@@ -707,7 +707,28 @@
 #     print("retry again")  
 
 
+# wap to print tringle in no pattern
+# no=1
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(no,end='')
+#         no+=1
+
+#     print()
 
 
-
+# wap to print diamond patten
+n=int(input("enter a no"))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end='')
+    for k in range(1,2*i):
+        print("*",end='')
+    print()
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print(" ",end='')
+    for k in range(1,2*i):
+        print("*",end='')
+    print()
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,13 @@
-
-for i in range (1,5):
-    for j in range(1,5-i):
-        print(" ",end="")    
-    for j in range((2*i)-1):
-        print("*",end="")
-    print("\n")  
-    
-    for j in range(i):
-        print(" ",end="")       
-    for j in range(2*(9-i)-1):
-        print("*",end="")
-    print("\n")    
+("enter a no"))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end='')
+    for k in range(1,2*i):
+        print("*",end='')
+    print()
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print(" ",end='')
+    for k in range(1,2*i):
+        print("*",end='')
+    print()
