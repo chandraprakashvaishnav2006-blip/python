8cdbd9aa5819413bbd9c5b2e8c7b60f7fe3d9b7f
chandra prakash vaishnav <chandraprakashvaishnav2006@gmail.com>
2026-08-03 21:56:58 +0530

    crash course

--- pythonfull.py
+++ pythonfull.py
@@ -718,17 +718,17 @@
 
 
 # wap to print diamond patten
-n=int(input("enter a no"))
-for i in range(1,n+1):
-    for j in range(1,n-i+1):
-        print(" ",end='')
-    for k in range(1,2*i):
-        print("*",end='')
-    print()
-for i in range(n-1,0,-1):
-    for j in range(1,n-i+1):
-        print(" ",end='')
-    for k in range(1,2*i):
-        print("*",end='')
-    print()
+# n=int(input("enter a no"))
+# for i in range(1,n+1):
+#     for j in range(1,n-i+1):
+#         print(" ",end='')
+#     for k in range(1,2*i):
+#         print("*",end='')
+#     print()
+# for i in range(n-1,0,-1):
+#     for j in range(1,n-i+1):
+#         print(" ",end='')
+#     for k in range(1,2*i):
+#         print("*",end='')
+#     print()
 
