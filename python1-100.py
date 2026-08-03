d9220348eb6619a4df023fc5d600b38d87346f31
chandra prakash vaishnav <chandraprakashvaishnav2006@gmail.com>
2026-08-03 23:07:33 +0530

    day1

---
+++ python1-100.py
@@ -0,0 +1,71 @@
+# 1_program
+# print("Hello, World!",7)
+# print(5)
+
+# \r move cursor to the beginning 
+# print("hello \rhi")
+# output: hillo
+
+
+# \b backspace
+# print("hello\bhi")
+# output: hellhi
+
+# to seperate the values with a custom separator, we can use sep parameter in print function
+# print( "Hey", 6, 7, sep="~")
+# output: Hey~6~7
+
+
+# end parameter is used to specify what to print at the end of the output. By default, it is a newline character.   
+# print( "Hey", 6, 7, sep="~", end="009")
+# print("hardik")
+
+
+
+# 14 types of data types in python
+# a1=2
+# a2=3.5
+# a3="hello"
+# a4=True
+# a5=2+3j
+# a6=[1,2,3]
+# a7=(1,2,3)
+# a8={1:2,3:4}
+# a9={1,2,3}  
+# a10=None
+# a11=bytearray(5)
+# a12=bytes(5)
+# a13=frozenset([1,2,3])
+# a14=range(5)
+# print(type(a1))
+# print(type(a2))
+# print(type(a3))
+# print(type(a4))
+# print(type(a5))
+# print(type(a6))
+# print(type(a7))
+# print(type(a8))
+# print(type(a9))
+# print(type(a10))
+# print(type(a11))
+# print(type(a12))
+# print(type(a13))
+# print(type(a14))
+
+
+# make calculator using if else in python
+# a=int(input("Enter first number: "))
+# b=int(input("enter no 2: "))
+# c=input ("Enter operator: ")
+# if c=="+":
+#     print(a+b)
+# elif c=="-":
+#     print(a-b)    
+# elif c=="*":
+#     print(a*b)
+# elif c=="/":
+#     print(a/b)
+# else:
+#     print("Invalid operator")
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,2 @@
-("enter a no"))
-for i in range(1,n+1):
-    for j in range(1,n-i+1):
-        print(" ",end='')
-    for k in range(1,2*i):
-        print("*",end='')
-    print()
-for i in range(n-1,0,-1):
-    for j in range(1,n-i+1):
-        print(" ",end='')
-    for k in range(1,2*i):
-        print("*",end='')
-    print()
+ello, World!",7)
+# print(5)
